0a2277387c1bc62864c9cf2c88f23f9a930242a3
Frank Fidanza <fidanza.frank@gmail.com>
2019-09-05 16:31:41 +0200

    Feature: awx

--- scripts/awx.sh
+++ scripts/awx.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-INSTALLDIR=/home/vagrant/awx
-
-# check if ansible is installed
-if ! [ -x "$(command -v ansible)" ] ; then
-    echo "Ansible needs to be installed before this script!"
-    exit 1
-fi
+INSTALL_DIR="/home/vagrant/awx"
 
 if ! [ -x "$(command -v docker)" ] ; then
     echo "Docker needs to be installed before this script!"
     exit 1
 fi
 
-git clone https://github.com/ansible/awx.git ${INSTALLDIR}
-pushd ${INSTALLDIR}/installer
+apt-get install python-pip
+pip install docker-compose
+
+# check if ansible is installed
+if ! [ -x "$(command -v ansible)" ] ; then
+    pip install ansible
+fi
+
+git clone https://github.com/ansible/awx.git ${INSTALL_DIR}
+pushd ${INSTALL_DIR}/installer
+sed -i -r "s/#?project_data_dir=.*/project_data_dir=\\/home\/vagrant\/ansible/g" inventory
 ansible-playbook -i inventory install.yml
 popd
