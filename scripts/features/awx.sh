b62d403d78c80d17f9ab76457165b0253f417cc4
Frank Fidanza <fidanza.frank@gmail.com>
2019-10-08 10:30:10 +0200

    Allow choosing which awx release is to be installed using env variable

--- scripts/features/awx.sh
+++ scripts/features/awx.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 INSTALL_DIR="/home/vagrant/awx"
 CUSTOM_AWX_CONFIG=/vagrant/awx_inventory
+VERSION=${AWX_VERSION:-"7.0.0"}
 
 if ! [ -x "$(command -v docker)" ] ; then
     echo "Docker needs to be installed before this script!"
@@ -16,12 +17,15 @@ if ! [ -x "$(command -v ansible)" ] ; then
     pip install ansible
 fi
 
-git clone https://github.com/ansible/awx.git ${INSTALL_DIR}
+wget -qO /tmp/awx.tar.gz https://github.com/ansible/awx/archive/${VERSION}.tar.gz
+mkdir -p ${INSTALL_DIR}
+tar -C ${INSTALL_DIR} -xzf /tmp/awx.tar.gz --strip-components 1
 
 if [ -f ${CUSTOM_AWX_CONFIG} ] ; then
     cp ${CUSTOM_AWX_CONFIG} ${INSTALL_DIR}/installer/inventory
 fi
 
+echo "AWX version $(cat ./{$INSTALL_DIR}/VERSION) is going to be installed..."
 pushd ${INSTALL_DIR}/installer
 ansible-playbook -i inventory install.yml
 popd
