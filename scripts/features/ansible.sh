6a7e8a19e93cd18f26eef25993044d1138f7f3e5
Frank Fidanza <fidanza.frank@gmail.com>
2019-10-08 10:08:43 +0200

    Fix slow ubuntu mirrors using pip instead

--- scripts/features/ansible.sh
+++ scripts/features/ansible.sh
@@ -2,9 +2,11 @@
 
 # install ansible
 if ! [ -x "$(command -v ansible)" ] ; then
-    add-apt-repository ppa:ansible/ansible-2.8
-    apt-get update
-    apt-get install -y ansible
+    # add-apt-repository --yes ppa:ansible/ansible-2.8
+    # apt-get update
+    # apt-get install -y ansible
+    apt-get install -y python-pip
+    pip install ansible
 else
     echo "Ansible was already installed, skipping..."
     exit 0
