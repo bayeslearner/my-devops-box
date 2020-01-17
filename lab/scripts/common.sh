6f8b3095bab286495fe69b5db3b8fd5b86a6d757
Frank Fidanza <fidanza.frank@gmail.com>
2020-01-17 13:32:32 +0100

    Add condition for ansible user creation

--- lab/scripts/common.sh
+++ lab/scripts/common.sh
@@ -2,26 +2,27 @@
 
 HOME=/home/ansible
 
-# create ansible user
-sudo useradd -p $(openssl passwd -1 ansible) --create-home -s /bin/bash ansible
+if [ ! id -u ansible ] ; then 
+    # create ansible user
+    sudo useradd -p $(openssl passwd -1 ansible) --create-home -s /bin/bash ansible
+    # add ansible user to ansible group
+    sudo usermod -a -G ansible ansible
 
-# add ansible user to ansible group
-sudo usermod -a -G ansible ansible
+    # add it to sudoers
+    echo "
+    Defaults:ansible  !requiretty
+    ansible   ALL=(ALL)   NOPASSWD: ALL
+    " | sudo tee /etc/sudoers.d/ansible
 
-# add it to sudoers
-echo "
-Defaults:ansible  !requiretty
-ansible   ALL=(ALL)   NOPASSWD: ALL
-" | sudo tee /etc/sudoers.d/ansible
-
-# validate
-sudo visudo -cf /etc/sudoers.d/ansible 
+    # validate
+    sudo visudo -cf /etc/sudoers.d/ansible 
+fi
 
 # set ssh keys
-sudo mkdir $HOME/.ssh/
+sudo mkdir -p $HOME/.ssh/
 echo "$ANSIBLE_PRIVKEY_CONTENT" | sudo tee $HOME/.ssh/$ANSIBLE_PRIVKEY_NAME > /dev/null
 sudo chmod 600 $HOME/.ssh/$ANSIBLE_PRIVKEY_NAME
-echo "$ANSIBLE_PUBKEY_CONTENT" | sudo tee $HOME/.ssh/authorized_keys > /dev/null
+echo "$ANSIBLE_PUBKEY_CONTENT" | sudo tee -a $HOME/.ssh/authorized_keys > /dev/null
 sudo chmod 600 $HOME/.ssh/authorized_keys
 
 # refresh permissions
