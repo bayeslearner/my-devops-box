21eef95a175a59de2f3665c6b85515d163e421a4
Frank Fidanza <fidanza.frank@gmail.com>
2020-02-13 13:55:58 +0100

    Update sample script

--- scripts/awx_job.sh
+++ scripts/awx_job.sh
@@ -6,14 +6,17 @@
 # export AWX_ACCESS_TOKEN=<token>
 
 ACCESS_TOKEN=${AWX_ACCESS_TOKEN}
-JOB_TEMPLATE_ID=${1:-"1"}
-AWX_HOST=${2:-"localhost"}
-AWX_PORT=${3:-"80"}
+JOB_TEMPLATE_ID=${1:-'1'}
+AWX_HOST=${2:-'192.168.1.5'}
+AWX_PORT=${3:-'443'}
+GIT_BRANCH=${4:-master}
+PAYLOAD=$(jq -n -c --arg branch $GIT_BRANCH '{ extra_vars: { git_branch: $branch } }') # Survey with git_branch variable must be set and enabled in job template settings 
 
 # Executes a job template which ID equals to $JOB_TEMPLATE_ID variable, and outputs the JSON response to stdout
 curl -k -H "Authorization: Bearer ${ACCESS_TOKEN}" \
     -H "Content-Type: application/json" \
-    -X POST http://${AWX_HOST}:${AWX_PORT}/api/v2/job_templates/${JOB_TEMPLATE_ID}/launch/ | jq
+    -d $PAYLOAD \
+    -X POST https://${AWX_HOST}:${AWX_PORT}/api/v2/job_templates/${JOB_TEMPLATE_ID}/launch/ | jq
 
 # You may also create a token from the command line, using the API
 # To create an application, go to http://<tower_host>/applications
