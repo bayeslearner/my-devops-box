22f13566237e449a79bb5d5ca6092bc97c4fc8c5
fidanf <fidanza.frank@gmail.com>
2020-04-25 21:10:16 +0200

    Upgrade pip. Prevent segfault. Add check for ansible

--- scripts/features/ansible.sh
+++ scripts/features/ansible.sh
@@ -3,8 +3,12 @@
 # Installs ansible along with other components
 apt-get install -y python3-pip
 ln -sf /usr/bin/python3 /usr/bin/python
-pip3 install --upgrade --ignore-installed --requirement /vagrant/requirements.txt
 ln -sf /usr/bin/pip3 /usr/bin/pip
+pip install --upgrade pip
+
+if ! [ -x "$(command -v ansible)" ] ; then
+    sudo -u vagrant pip install --upgrade --ignore-installed --requirement /vagrant/requirements.txt
+fi
 
 # Display current ansible version
 ansible --version
