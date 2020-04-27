495b591149e86d0ba9c493d55ddb0bc6cc5f5562
fidanf <fidanza.frank@gmail.com>
2020-04-27 16:05:59 +0200

    Remove check & sudo. Apt testresources & cryptography packages

--- scripts/features/ansible.sh
+++ scripts/features/ansible.sh
@@ -1,14 +1,11 @@
 #!/bin/bash -e
 
 # Installs ansible along with other components
-apt-get install -y python3-pip
+apt-get install -y python3-pip python3-testresources
 ln -sf /usr/bin/python3 /usr/bin/python
 ln -sf /usr/bin/pip3 /usr/bin/pip
 pip install --upgrade pip
-
-if ! [ -x "$(command -v ansible)" ] ; then
-    sudo -u vagrant pip install --upgrade --ignore-installed --requirement /vagrant/requirements.txt
-fi
+pip install --upgrade --ignore-installed --requirement /vagrant/requirements.txt
 
 # Display current ansible version
 ansible --version
