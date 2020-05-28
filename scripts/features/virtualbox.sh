8d7624b5a5207aee2ea7c74a204abedcffd25e1f
fidanf <fidanza.frank@gmail.com>
2020-05-28 11:58:02 +0200

    Initial commit

--- scripts/features/virtualbox.sh
+++ scripts/features/virtualbox.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e 
 
-if which VBoxManage > /dev/null ; then
+if ! which VBoxManage > /dev/null ; then
     # Install VirtualBox
     wget -q -O- http://download.virtualbox.org/virtualbox/debian/oracle_vbox_2016.asc | sudo apt-key add -
     echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
     apt-get update
-    apt-get install virtualbox-6.1
+    apt-get install -y virtualbox-6.1
     usermod -aG vboxusers vagrant
     VBoxManage --version
+else
+    echo "VirtualBox was already installed, skipping..."
+    exit 0
 fi
