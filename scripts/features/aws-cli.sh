0dce77d80399fa5ac6386f988ee37507ae2e72ab
Frank Fidanza <fidanza.frank@gmail.com>
2020-10-12 11:13:19 +0200

    update condition

--- scripts/features/aws-cli.sh
+++ scripts/features/aws-cli.sh
@@ -1,7 +1,7 @@
-#!/bin/bash -ux
+#!/bin/bash -eu
 
 # install awscli
-if ! [ -x "$(command -v aws)" ] ; then
+if ! which aws > /dev/null ; then
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o /tmp/awscliv2.zip
   unzip /tmp/awscliv2.zip
   bash ./aws/install
